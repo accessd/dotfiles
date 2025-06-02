0e08e8cc34f37554589dbbf979796886597f9285
Andrey Morskov <accessd0@gmail.com>
2025-06-02 21:31:35 +0300

    yabai clean spaces script

---
+++ files/bin/cleanEmptySpaces.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+yabai -m query --spaces --display | \
+     jq -re 'map(select(."native-fullscreen" == 0)) | length > 1' \
+     && yabai -m query --spaces | \
+          # jq -re 'map(select(."windows" == [] and ."focused" == 0 and ."label" == "").index) | reverse | .[] ' | \
+          jq -re 'map(select(."focused" == 0).index) | reverse | .[] ' | \
+          xargs -I % sh -c 'yabai -m space % --destroy'
