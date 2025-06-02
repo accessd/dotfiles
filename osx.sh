8031ae628d24cc66968706ad9bf3f3f949b73a2b
Andrey Morskov <accessd0@gmail.com>
2025-06-02 19:48:07 +0300

    add osx script

---
+++ osx.sh
@@ -0,0 +1,81 @@
+echo "Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "show hidden files by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool false
+
+echo "expand save dialog by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# echo "show the ~/Library folder in Finder"
+chflags nohidden ~/Library
+
+echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "Disable window animations and Get Info animations in Finder"
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+echo "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Don’t animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false
+
+#echo "Disable press-and-hold for keys in favor of key repeat"
+#defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+echo "Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo "Disable opening and closing window animations"
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+echo "Avoid creating .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+echo "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# echo "Disable the warning before emptying the Trash"
+# defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Empty Trash securely by default
+# defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+#echo "Require password immediately after sleep or screen saver begins"
+#defaults write com.apple.screensaver askForPassword -int 1
+#defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo "Enable tap to click (Trackpad)"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Disable send and reply animations in Mail.app
+# defaults write com.apple.Mail DisableReplyAnimations -bool true
+# defaults write com.apple.Mail DisableSendAnimations -bool true
+
+# Disable Resume system-wide
+# defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# echo "Disable the “reopen windows when logging back in” option"
+# This works, although the checkbox will still appear to be checked.
+# defaults write com.apple.loginwindow TALLogoutSavesState -bool false
+# defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
+
+# echo "Disable local Time Machine backups"
+# hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+echo "Kill affected applications"
+for app in Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
