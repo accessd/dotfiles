ab1798a416c7af1a4389d5127e8b0a0800f30638
Andrey Morskov <accessd0@gmail.com>
2025-06-16 21:09:22 +0300

    nvim: update mini statuline

--- nvim/lua/kickstart/plugins/mini.lua
+++ nvim/lua/kickstart/plugins/mini.lua
@@ -20,17 +20,21 @@ return {
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      -- local statusline = require 'mini.statusline'
-      -- -- set use_icons to true if you have a Nerd Font
-      -- statusline.setup { use_icons = vim.g.have_nerd_font }
-      --
-      -- -- You can configure sections in the statusline by overriding their
-      -- -- default behavior. For example, here we set the section for
-      -- -- cursor location to LINE:COLUMN
-      -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return '%2l:%-2v'
-      -- end
+      local statusline = require 'mini.statusline'
+
+      require('mini.statusline').setup({
+        use_icons = vim.g.have_nerd_font,
+      })
+
+      vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal', { fg = '#ffffff', bg = '#005f87', bold = true })
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
